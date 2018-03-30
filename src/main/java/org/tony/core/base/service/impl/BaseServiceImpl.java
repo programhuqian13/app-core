12da798fbabd4ca070daf0dbec8f262b4a396fe8
xuanyi <xuanyi@baofu.com>
2018-03-30 18:06:52 +0800

    代码完善

--- src/main/java/org/tony/core/base/service/impl/BaseServiceImpl.java
+++ src/main/java/org/tony/core/base/service/impl/BaseServiceImpl.java
@@ -3,7 +3,7 @@ package org.tony.core.base.service.impl;
 import org.tony.core.base.service.BaseService;
 
 /**
- * @Description 描述
+ * @Description 相应的实现
  * @Version 1.0
  * @Author tony
  * @Date 2018/3/2
@@ -12,6 +12,6 @@ import org.tony.core.base.service.BaseService;
  */
 public abstract class BaseServiceImpl<T,ID> implements BaseService<T,ID>{
 
-    
+
 
 }
---
+++ src/main/java/org/tony/core/base/web/BaseController.java
@@ -0,0 +1,12 @@
+package org.tony.core.base.web;
+
+/**
+ * @Description 控制器的base
+ * @Version 1.0
+ * @Author xuanyi@baofu.com
+ * @Date 2018/3/30
+ * @ProjectName app-core
+ * @PackageName org.tony.core.base.web
+ */
+public class BaseController {
+}
